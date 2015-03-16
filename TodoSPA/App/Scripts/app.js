32f2fd656b4d54da04150cb11a9e40765b7bf392
Danny Strockis <dastrock@microsoft.com>
2015-03-15 19:42:35 -0700

    Update app.js

--- TodoSPA/App/Scripts/app.js
+++ TodoSPA/App/Scripts/app.js
@@ -31,7 +31,8 @@ angular.module('todoApp', ['ngRoute','AdalAngular'])
             clientId: 'Enter your client ID here e.g. e9a5a8b6-8af7-4719-9821-0deef255f68e',
             extraQueryParameter: 'nux=1',
             endpoints: endpoints,
-            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+            //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.  
+            // Also, token acquisition for the To Go API will fail in IE when running on localhost, due to IE security restrictions.
         },
         $httpProvider
         );
